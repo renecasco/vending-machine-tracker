9bbecfe5cfac44f4d1e1c7a1e12e2939b06da37f
Rene Casco <rene.casco@icloud.com>
2019-02-15 10:48:06 -0700

    adds feature test for user story 1

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,16 @@ feature 'When a user visits a vending machine show page' do
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario 'they see the name of all of the snacks associated with that vending machine along with their price' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1= dons.snacks.create(name:"White Castle Burger", price: 3.50)
+    snack_2= dons.snacks.create(name:"Pop Rocks", price: 1.50)
+    snack_3= dons.snacks.create(name:"Flaming Hot Cheetos", price: 2.50)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+  end
 end
